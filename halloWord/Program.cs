b91ba9a35f0a1c7ef3bfa944b15efaedd00854eb
Magdalena_Maria <Magdalena_Maria@SPOTLIGHT>
2024-01-02 18:36:56 +0100

    Add project files.

---
+++ halloWord/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+    namespace Hello Word
+{
+ class Program
+{
+
+static vold Main(string[]args)
+
+}
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("nazywam sie Magdalena Kilarska");
+
